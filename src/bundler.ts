3de31754813ed37c38b70e9453a0a9837d4a6aa4
John Letey <john@kyve.network>
2021-11-26 09:49:09 +0000

    refactor: wait for `toHeight` to be mined

--- src/bundler.ts
+++ src/bundler.ts
@@ -1,18 +1,33 @@
+import { BlockWithTransactions } from "@ethersproject/abstract-provider";
 import { BundlerFunction } from "@kyve/core/dist/src/faces";
 import { ethers } from "ethers";
 import { ConfigType } from "./faces";
+
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 const bundlerFunction: BundlerFunction<ConfigType> = async (
   config,
   fromHeight,
   toHeight
 ) => {
-  const bundle: any[] = [];
+  const bundle: BlockWithTransactions[] = [];
   const provider = new ethers.providers.StaticJsonRpcProvider(config.rpc);
 
-  // TODO: handle scenario where toHeight is higher than current block height
-  // TODO: maybe implement sleep in order not to get rate limited
+  const waitForBlock = async (height: number): Promise<void> => {
+    return new Promise(async (resolve) => {
+      let currentHeight = await provider.getBlockNumber();
+      while (currentHeight < height) {
+        await sleep(10 * 1000);
+        currentHeight = await provider.getBlockNumber();
+      }
 
+      resolve();
+    });
+  };
+
+  await waitForBlock(toHeight);
   for (let height = fromHeight; height < toHeight; height++) {
     const block = await provider.getBlockWithTransactions(height);
     bundle.push(block);
