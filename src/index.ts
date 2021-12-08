be5da2e5a44dff6a55d001b649699bb7efa1c2d4
fabianriewe <fabian@kyve.network>
2021-12-08 14:17:57 +0100

    feat: added metric server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import KYVE from "@kyve/core";
 import { version } from "../package.json";
 import bundler from "./bundler";
+import { server } from "@kyve/core/dist/src/registry";
 
 process.env.KYVE_RUNTIME = "@kyve/evm";
 process.env.KYVE_VERSION = version;
@@ -8,4 +9,5 @@ process.env.KYVE_VERSION = version;
 (async () => {
   const { node } = await KYVE.generate();
   node.start(bundler);
+  server.listen(8080)
 })();
